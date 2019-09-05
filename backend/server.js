e56896d13aab995b0bc7d09449aadde3216357e8
Adam Sinning <asinning25@gmail.com>
2019-09-05 13:54:05 -0600

    connected create user to backend

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
